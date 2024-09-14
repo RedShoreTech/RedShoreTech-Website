70c2e37bfbe6df4b5dd02407e0f8e5bde3507213
donchiu <donchiu@redshore.space>
2024-09-14 19:41:25 +0000

    Do not need frp to publish hugo server

--- tools/service/setup-services.sh
+++ tools/service/setup-services.sh
@@ -1,16 +1,16 @@
-cp frps.ini /usr/local/frps/
-cp frpc-hugo.ini /usr/local/frps/
+##cp frps.ini /usr/local/frps/
+##cp frpc-hugo.ini /usr/local/frps/
+##cp frpc-hugo.service /etc/systemd/system/
+##cp frps.service /lib/systemd/system/
 
 cp web-redshore.service /etc/systemd/system/
-cp frpc-hugo.service /etc/systemd/system/
-cp frps.service /lib/systemd/system/
 
 sudo systemctl daemon-reload
-sudo systemctl enable frps
-sudo systemctl enable frpc-hugo
-sudo systemctl enable web-redshore
+##sudo systemctl enable frps
+##sudo systemctl enable frpc-hugo
+##sudo systemctl enable web-redshore
 
-sudo systemctl start frps
-sudo systemctl start frpc-hugo
+##sudo systemctl start frps
+##sudo systemctl start frpc-hugo
 sudo systemctl start web-redshore
 
